2cf8cf060e35cee3784de112f6488a6bf0566fc9
T0Mage <tompraszkiewicz@gmail.com>
2020-05-06 07:47:27 +0200

    added zad4

--- source/main.cpp
+++ source/main.cpp
@@ -5,10 +5,12 @@
 #include <cmath>
 #include <fstream>
 
-void project_1() {
+#include <queue>
+
+void project_1()
+{
     std::ofstream file("output.dat");
     Graph *graph;
-
 
     AdjacencyMatrix adjacencyMatrix = AdjacencyMatrix::loadFromFile("input/p1/adjMat.txt");
     adjacencyMatrix.print(std::cout);
@@ -24,8 +26,8 @@ void project_1() {
 
     graph = &adjacencyMatrix;
 
-    AdjacencyList testAdjacencyList({{},{}});
-    IncidenceMatrix testIncidenceMatrix({{},{}});
+    AdjacencyList testAdjacencyList({{}, {}});
+    IncidenceMatrix testIncidenceMatrix({{}, {}});
 
     Graph::convert(*graph, testIncidenceMatrix);
     Graph::convert(*graph, testAdjacencyList);
@@ -34,17 +36,18 @@ void project_1() {
     std::cout << "\n\n";
     testIncidenceMatrix.print(std::cout);
     std::cout << "\n\n";
-    
+
     adjacencyList = Graph::randomByEdges(6, 9);
     adjacencyList = Graph::randomByProbability(6, 0.3);
     graph = &adjacencyList;
-    graph -> printToFile(file);
+    graph->printToFile(file);
     std::cout << graph;
 }
 
-void project_2() {
+void project_2()
+{
     // zadanie 1
-    
+
     // std::vector<int> A1 = {3,2,1,0}; // nie
     // AdjacencyList::constuctGraphFromDegreeSequence(A1);
     // std::vector<int> A2 = {3,3,3,3}; // tak
@@ -58,7 +61,7 @@ void project_2() {
     // graph -> printToFile(file);
 
     // zadanie 2
-    
+
     // std::ofstream file1("output-p2-z2-1.dat"); //Zmienić gnuplot.sh przy pokazywaniu!
     // std::ofstream file2("output-p2-z2-2.dat");
     // AdjacencyList adjacencyList = AdjacencyList::loadFromFile("input/p1/adjList.txt");
@@ -72,7 +75,7 @@ void project_2() {
     // std::cout << std::endl;
 
     // zadanie 3
-    
+
     // std::vector<int> A4 = {4,2,2,3,2,1,4,2,2,2,2}; // tak
     // AdjacencyList adjacencyList = Graph::constuctGraphFromDegreeSequence(A4);
     // Graph::largestComponent(adjacencyList);
@@ -95,7 +98,7 @@ void project_2() {
     // std::ofstream file("output.dat");
     // graph -> printToFile(file);
     // g1.print(std::cout);
-    // Graph::findEulerCycle(g1); 
+    // Graph::findEulerCycle(g1);
     // Graph::randomEuler(10);
 
     // zadanie 5
@@ -118,10 +121,9 @@ void project_2() {
     //     0,0,0,0,1,1,1,0
     //                     );
     // Graph::hamiltonCycleFind(z6);
-    
 }
 
-void project_3() 
+void project_3()
 {
 
     // Zadanie 1
@@ -138,13 +140,45 @@ void project_3()
 
     //Zadanie 3
 
-    for(int vertex = 0; vertex < gr -> getVertexAmount(); vertex++)
+    for (int vertex = 0; vertex < gr->getVertexAmount(); vertex++)
     {
         std::vector<int> result = Graph::dijkstraAlgorithm(*gr, vertex, false);
-        for(auto i: result)
-            std::cout << i <<"\t";
+        for (auto i : result)
+            std::cout << i << "\t";
         std::cout << std::endl;
     }
+
+    //Zadanie 4
+    struct ve
+    {
+        ve(int v, int w) : v(v), w(w){};
+        int v, w;
+        bool operator>(ve a) { return w > a.w; }
+    };
+    std::priority_queue<ve, std::vector<ve>, std::greater<>> min_sums;
+    std::priority_queue<ve, std::vector<ve>, std::greater<>> minmax;
+    std::cout << "\n\nSumy wag wierzchłków:" << std::endl;
+    for (int vertex = 0; vertex < gr->getVertexAmount(); vertex++)
+    {
+        std::vector<int> result = Graph::dijkstraAlgorithm(*gr, vertex, false);
+        int sum = 0;
+        int max = 0;
+        for (auto i : result)
+        {
+            sum += i;
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+        min_sums.push(ve(vertex, sum));
+        minmax.push(ve(vertex, max));
+        std::cout << vertex << ": " << sum << std::endl; //show all sums
+    }
+    std::cout << "Najmniejsza suma wag: " << std::endl;
+    std::cout << min_sums.top().v << ": " << min_sums.top().w << std::endl;
+    std::cout << "Centrum minmax: " << std::endl;
+    std::cout << minmax.top().v << ": " << minmax.top().w << std::endl<< std::endl<< std::endl;
 }
 
 int main()
